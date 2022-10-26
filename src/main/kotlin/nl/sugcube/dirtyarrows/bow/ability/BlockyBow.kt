2d2d9bb46e318f30e345c540a4f06f5d57090a19
SugarCaney <sugarcaneydearie@gmail.com>
2022-10-26 14:51:12 +0200

    Updated blocky bow to 1.13
    Removed block data - everything has been flattened into block materials.

--- src/main/kotlin/nl/sugcube/dirtyarrows/bow/ability/BlockyBow.kt
+++ src/main/kotlin/nl/sugcube/dirtyarrows/bow/ability/BlockyBow.kt
@@ -9,6 +9,7 @@ import org.bukkit.Material
 import org.bukkit.entity.Arrow
 import org.bukkit.entity.Player
 import org.bukkit.event.entity.ProjectileHitEvent
+import org.bukkit.inventory.ItemStack
 
 /**
  * Places blocks from the offhand on the location of impact.
@@ -23,7 +24,6 @@ open class BlockyBow(plugin: DirtyArrows) : BowAbility(
         description = "Places blocks from your off-hand."
 ) {
 
-    @Suppress("DEPRECATION")
     override fun land(arrow: Arrow, player: Player, event: ProjectileHitEvent) {
         val itemToPlace = player.itemInOffHand
         if (itemToPlace.type.isBlock.not() || itemToPlace.type == Material.AIR) return
@@ -31,30 +31,26 @@ open class BlockyBow(plugin: DirtyArrows) : BowAbility(
         val block = arrow.location.block
         if (block.type != Material.AIR) return
 
-        /*block.type = itemToPlace.type
-        block.data = itemToPlace.data.data
-         */
-        // TODO: New block data system Blocky Bow (block)
+        // No block data needed since the flattening.
+        // Ignore items - it's a 'blocky' bow, so only works for blocks.
+        // Yes, this is an easy way of not having to implement that.
 
+        block.type = itemToPlace.type
         player.removeBlockItem(itemToPlace.type)
     }
 
     /**
      * Removes a block of the given type from the inventory, do nothing if the item should not be removed.
      */
-    @Suppress("DEPRECATION")
     private fun Player.removeBlockItem(type: Material) {
         if (gameMode == GameMode.CREATIVE) return
 
         val offHandItem = itemInOffHand
         val newCount = offHandItem.amount - 1
-        val itemData = offHandItem.data
 
-        /*if (newCount == 0) {
+        if (newCount == 0) {
             inventory.setItemInOffHand(null)
         }
-        else inventory.itemInOffHand = ItemStack(type, newCount, itemData.data.toShort())
-         */
-        // TODO: New block data system Blocky Bow (item)
+        else inventory.setItemInOffHand(ItemStack(type, newCount))
     }
 }
